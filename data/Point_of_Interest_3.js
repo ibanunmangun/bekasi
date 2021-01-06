7404544a991c068eaa71f7553b5e5991b3aab8f6
ibanunmangun <62372980+ibanunmangun@users.noreply.github.com>
2021-01-06 13:58:06 +0700

    Add files via upload

---
+++ data/Point_of_Interest_3.js
@@ -0,0 +1,26 @@
+var json_Point_of_Interest_3 = {
+"type": "FeatureCollection",
+"name": "Point_of_Interest_3",
+"crs": { "type": "name", "properties": { "name": "urn:ogc:def:crs:OGC:1.3:CRS84" } },
+"features": [
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Pendopo alun-alun bekasi", "X": 106.9995346, "Y": -6.2417723, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.999534600000061, -6.2417723 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Masjid Al - Barkah", "X": 106.9995994, "Y": -6.2408237, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.999599400000079, -6.240823699999964 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Gedung Papak", "X": 107.0057373, "Y": -6.2442335, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.005737300000078, -6.2442335 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Gang Plebisit", "X": 107.00425, "Y": -6.24075, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.00425, -6.24075 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Tugu K.H. Agus Salim", "X": 107.0051111, "Y": -6.2389167, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 107.005111100000079, -6.238916699999947 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Warung Mpok Nini | Pusat Oleh-oleh Khas Betawi ( Oleh-oleh Khas Bekasi \/ Oleh oleh Khas Jakarta )", "X": 106.983295, "Y": -6.243352, "Keterangan": "Point of Interest" }, "geometry": { "type": "Point", "coordinates": [ 106.983295, -6.243352 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Pertamina 34.17106", "X": 107.001023, "Y": -6.238029, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.001023, -6.238029 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Lany Dental Care", "X": 107.000325, "Y": -6.250341, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.000325, -6.250341 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "PMI Kota Bekasi", "X": 107.000184, "Y": -6.238968, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.000184, -6.238968 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Apotek Kimia Farma Galuh", "X": 107.002387, "Y": -6.239572, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.002387, -6.239572 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Dokter Gigi Bekasi - Dokter Gigi Kita", "X": 107.004909, "Y": -6.243004, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.004909, -6.243004 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Pos Polisi Bekasi", "X": 107.000412, "Y": -6.237434, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.000412, -6.237434 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Masjid Nurul Amal Pusat Plebisit", "X": 107.005153, "Y": -6.242066, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.005153, -6.242066 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Masjid Jami Al-Akhyar", "X": 107.000546, "Y": -6.248833, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.000546, -6.248833 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "GKPI Bekasi", "X": 106.990481, "Y": -6.243193, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.990481, -6.243193 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Masjid Jami At-Taqwa", "X": 106.989462, "Y": -6.243668, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 106.989462, -6.243668 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "SPBU Pertamina", "X": 107.000077, "Y": -6.249757, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.000077, -6.249757 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "Apotik Prima Jaya", "X": 107.003097, "Y": -6.244451, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.003097, -6.244451 ] } },
+{ "type": "Feature", "properties": { "OID_": "0", "Name": "RS Bani Saleh Bekasi", "X": 107.003385, "Y": -6.245999, "Keterangan": "Fasilitas Umum" }, "geometry": { "type": "Point", "coordinates": [ 107.003385, -6.245999 ] } }
+]
+}
